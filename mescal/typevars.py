81d4e8c06b30bdd666a9066b9e04cbe054d81f00
helgestefan <on@hstes.de>
2025-08-04 10:16:31 +0200

    fixes ContinuousMappingType

--- mescal/typevars.py
+++ mescal/typevars.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
     from mescal.visualizations.folium_viz_system.base_viz_system import ResolvedFeature, FeatureResolver
     from mescal.visualizations.value_mapping_system.base import BaseMapping
     from mescal.visualizations.value_mapping_system.discrete import DiscreteInputMapping
-    from mescal.visualizations.value_mapping_system.continuous import SegmentedContinuousInputToContinuousOutputMapping
+    from mescal.visualizations.value_mapping_system.continuous import SegmentedContinuousInputMappingBase
     from mescal.visualizations.folium_legend_system.base import BaseLegend
 
 FlagType = TypeVar('FlagType', bound='FlagTypeProtocol')
@@ -26,5 +26,5 @@ FeatureResolverType = TypeVar('FeatureResolverType', bound='FeatureResolver')
 ResolvedFeatureType = TypeVar('ResolvedFeatureType', bound='ResolvedFeature')
 ValueMappingType = TypeVar('ValueMappingType', bound='BaseMapping')
 DiscreteMappingType = TypeVar('DiscreteMappingType', bound='DiscreteInputMapping')
-ContinuousMappingType = TypeVar('ContinuousMappingType', bound='SegmentedContinuousInputToContinuousOutputMapping')
+ContinuousMappingType = TypeVar('ContinuousMappingType', bound='SegmentedContinuousInputMappingBase')
 FoliumLegendType = TypeVar('FoliumLegendType', bound='BaseLegend')
