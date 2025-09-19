c03fb68970a1ba866d32f8e405fbe1c9e19868cf
helgestefan <on@hstes.de>
2025-09-19 13:14:04 +0200

    circumvents "FutureWarning highly fragmented df...."

--- mescal/utils/pandas_utils/set_new_column.py
+++ mescal/utils/pandas_utils/set_new_column.py
@@ -8,27 +8,29 @@ def set_column(
         new_column_values: pd.Series | pd.DataFrame
 ) -> pd.DataFrame:
 
-    if not len(df) == len(new_column_values):
-        raise ValueError('Length of df and new_column_values must be equal.')
+    dff = df.copy()
+
+    if not len(dff) == len(new_column_values):
+        raise ValueError('Length of dff and new_column_values must be equal.')
 
     # TODO optional: check index
 
     if isinstance(new_column_values, pd.Series):
-        df[new_column_name] = new_column_values
-        return df
+        dff[new_column_name] = new_column_values
+        return dff
 
     if isinstance(new_column_values, pd.DataFrame):
-        if not new_column_values.columns.nlevels == (df.columns.nlevels - 1):
+        if not new_column_values.columns.nlevels == (dff.columns.nlevels - 1):
             raise ValueError(
-                'Your new_column_values must have n-1 column levels, where n is the number of levels in df.'
+                'Your new_column_values must have n-1 column levels, where n is the number of levels in dff.'
             )
 
-        if new_column_name in df.columns:
-            df = df.drop(columns=[new_column_name])
+        if new_column_name in dff.columns:
+            dff = dff.drop(columns=[new_column_name])
 
-        new_column_values = pd.concat({new_column_name: new_column_values}, axis=1, names=[df.columns.names[0]])
-        df = pd.concat([df, new_column_values], axis=1)
-        return df
+        new_column_values = pd.concat({new_column_name: new_column_values}, axis=1, names=[dff.columns.names[0]])
+        dff = pd.concat([dff, new_column_values], axis=1)
+        return dff
 
     else:
         raise TypeError('Used new_column_values type not accepted.')
